364804f85192bfa5cc02f5b8c1a6657e77564180
shenalsj <shenal.sj@gmail.com>
2021-11-22 17:48:20 +0200

    commit1

---
+++ _helpers/constant.js
@@ -0,0 +1,11 @@
+module.exports.SECRET_OR_KEY = "website123finlandmasterssl"
+module.exports.HOST ="localhost"
+module.exports.USER = "root"
+module.exports.PASSWORD = "password"
+module.exports.DB = "db1"
+
+module.exports.USER_C = "root"
+module.exports.PASSWORD_C = "password"
+module.exports.DB_C = "paikkaprojekti"
+
+//constants varibles used in this project 
